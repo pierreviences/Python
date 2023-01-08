fb28bcab3b12a8ad71a4f46ebd80fb9dba13e0b5
Muhamad Yazid Imani <imaniyazid@gmail.com>
2023-01-08 20:51:03 +0700

    Dictionary in python

---
+++ Dasar/4. Fungsi/TipeData.py
@@ -0,0 +1,43 @@
+''' Type hints untuk fungsi '''
+
+# bentuk standar fungsi yang udah kita pelajari
+
+'''
+studi kasus
+def fungsi(parameter):
+    hasil = parameter**2
+    print(hasil)
+fungsi(1)
+fungsi("Ucup")
+fungsi(True)
+'''
+
+# penggunaan type hints
+
+'''
+contoh kalo di c++
+int kuadrat(int a){
+    a = a**2
+    return a
+}
+'''
+
+
+
+
+import string
+def sepuluh_pangkat(argument: int) -> int:
+    '''FUNGSI DENGAN HINTS'''
+    output = 10**argument
+    return output
+
+
+HASIL = sepuluh_pangkat(4)
+print(HASIL)
+
+
+def display(argument: string):
+    print(argument)
+
+
+display("Ucup")
