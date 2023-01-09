df84d510de27d92de5eb197dd7f0dd7450680e7a
Muhamad Yazid Imani <imaniyazid@gmail.com>
2023-01-09 16:58:40 +0700

    Import in python

---
+++ Dasar/5. import/Main.py
@@ -0,0 +1,7 @@
+import bangun_datar as bd
+
+luas_persegi = bd.luas_persegi(5)
+print(f"Luas Persegi : {luas_persegi}")
+
+keliling_persegi = bd.keliling_persegi(5)
+print(f"Keliling Persegi : {keliling_persegi}")
---
+++ Dasar/5. import/bangun_datar.py
@@ -0,0 +1,6 @@
+def luas_persegi(s: int) -> int:
+    return s*s
+
+
+# keliling_persegi = lambda s:s*4
+def keliling_persegi(s): return s*4
