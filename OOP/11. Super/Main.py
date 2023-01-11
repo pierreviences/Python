c874739a1e2198efa5db939b58f5602a93dc2d6d
Muhamad Yazid Imani <imaniyazid@gmail.com>
2023-01-11 08:33:21 +0700

    Super in python

---
+++ OOP/11. Super/Main.py
@@ -0,0 +1,28 @@
+class Hero:
+    def __init__(self, name, health):
+        self.name = name
+        self.health = health
+
+    def showInfo(self):
+        print("{} dengan health: {}".format(self.name, self.health))
+
+
+class Hero_intelligent(Hero):
+    def __init__(self, name):
+        # cara 1 -> tidak disarankan, karena jika nama parentny diganti, harus diganti juga
+        # Hero.__init__(self, name, 100)
+        # Hero.showInfo(self)
+
+        # disarankan pake cara yg kedua ini
+        super().__init__(name, 100)
+        super().showInfo()
+
+
+class Hero_strength(Hero):
+    def __init__(self, name):
+        super().__init__(name, 200)
+        super().showInfo()
+
+
+lina = Hero_intelligent('lina')
+axe = Hero_strength('axe')
